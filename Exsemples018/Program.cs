3399419b6a71ac3d30927aa39f18f159a0e297a2
Andrey Pakulov <anpa1984@yandex.ru>
2022-09-10 09:49:33 +0200

    Exsemples 018

---
+++ Exsemples018/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает 
+// диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(GetDiaposon(num));
+
+string GetDiaposon(int quater)
+{
+    string result;
+    if(quater == 1)
+    {
+        result = "x > 0 и y > 0";
+    }
+    else if(quater ==2)
+    {
+         result = "x < 0 и y > 0";
+    }
+    else if(quater == 3)
+     {
+         result = "x < 0 и y < 0";
+    }
+    else
+    {
+         result = "x > 0 и y < 0";
+    }
+    return result;
+}
